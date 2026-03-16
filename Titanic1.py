5dc36a1574cd859ec2d054505872a6a8f6cafe96
PAVITHRA K <pavithrakpavi26@gmail.com>
2026-03-16 12:07:58 +0530

    Add files via upload

---
+++ Titanic1.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+data = pd.read_csv(r"C:\Users\pavit\OneDrive\Desktop\internship\DAY32\Titanic-Dataset.csv")
+
+X = data[['Pclass','Age','Fare']].fillna(0)
+y = data['Survived']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+model = DecisionTreeClassifier(criterion="entropy", max_depth=3)
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+print(predictions[:5])
